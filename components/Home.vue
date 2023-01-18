2641dbf92b2aa6d95b3c13591e0eb1b6a12ccc0e
reidelfin <reiameramos@gmail.com>
2023-01-18 16:20:52 +0800

    test

---
+++ components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div style="background-color: lightcyan">
+      <h2 style="text-align: center">Home Page</h2>
+      <Sidebar/>
+      <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh">
+  
+      </div>
+    </div>
+  </template>
+  
+  <script >
+  import Sidebar from './Sidebar';
+  export default {
+    name: 'HomePage',
+    components: {
+      Sidebar,
+    },
+  }
+  </script>
